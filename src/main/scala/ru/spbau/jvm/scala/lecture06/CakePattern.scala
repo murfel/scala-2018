9c1a5b884462e156aaf85acf4b3d1699a5ff7027
Andrew Kozlov <adkozlov@gmail.com>
2018-11-21 13:29:40 +0300

    lecture 06 added

---
+++ src/main/scala/ru/spbau/jvm/scala/lecture06/CakePattern.scala
@@ -0,0 +1,54 @@
+package ru.spbau.jvm.scala.lecture06
+
+object CakePattern {
+
+  trait FooComponent {
+
+    val foo: Foo
+
+    protected trait Foo {
+      def foo: String
+    }
+
+  }
+
+  trait FooComponentImpl extends FooComponent {
+    override val foo: Foo = new FooImpl
+
+    private class FooImpl extends Foo {
+      override def foo: String = "foo"
+    }
+
+  }
+
+  trait BarComponent {
+
+    val bar: Bar
+
+    protected trait Bar {
+      def bar: String
+    }
+
+  }
+
+  trait BarComponentImpl extends BarComponent {
+    override val bar: Bar = new BarImpl
+
+    private class BarImpl extends Bar {
+      override def bar: String = "bar"
+    }
+
+  }
+
+  trait Baz {
+
+    this: FooComponent with BarComponent => // self: FooComponent with BarComponent =>
+
+    def baz: String = foo.foo + bar.bar
+  }
+
+  def main(args: Array[String]): Unit = {
+    val baz = new Baz with FooComponentImpl with BarComponentImpl
+    println(baz.baz)
+  }
+}
---
+++ src/main/scala/ru/spbau/jvm/scala/lecture06/TryWithResources.scala
@@ -0,0 +1,26 @@
+package ru.spbau.jvm.scala.lecture06
+
+import java.io.{BufferedReader, FileNotFoundException, FileReader, IOException}
+
+object TryWithResources {
+
+  def main(args: Array[String]): Unit = args match {
+    case Array(head) =>
+      withClose(new BufferedReader(new FileReader(head))) { reader =>
+        var line = reader.readLine()
+        while (line != null) {
+          println(line)
+          line = reader.readLine()
+        }
+      } {
+        case e: FileNotFoundException =>
+          println(e.getMessage)
+        case e: IOException =>
+          for {
+            exception <- e +: e.getSuppressed
+            message = exception.getMessage
+          } println(message)
+      }
+    case _ =>
+  }
+}
