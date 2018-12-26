35cc6aaa5cf170476dc81c3d9a4d4167902e407f
murfel <murnatty@gmail.com>
2018-12-27 01:51:36 +0300

    refactor Number

--- src/main/scala/HList/Number.scala
+++ src/main/scala/HList/Number.scala
@@ -3,13 +3,10 @@ package HList
 sealed trait Number
 
 object Number {
-  sealed trait NonNegative extends Number
+  case object Zero extends Number
+  case class Successor[Predecessor <: Number](predecessor: Predecessor) extends Number
 
-  case object O extends NonNegative
-  case class Successor[Predecessor <: NonNegative](predecessor: Predecessor)
-    extends NonNegative
-
-  val _0: O.type = O
+  val _0: Zero.type = Zero
   val _1 = Successor(_0)
   val _2 = Successor(_1)
   val _3 = Successor(_2)
--- src/main/scala/HList/Splittable.scala
+++ src/main/scala/HList/Splittable.scala
@@ -9,12 +9,12 @@ object Splittable {
   import HList.{HNil, HCons}
 
   implicit def zeroSplittable[List <: HList]:
-  Splittable[List, Number.O.type, HList.HNil.type, List] =
-    (list: List, _: Number.O.type) => (HNil, list)
+  Splittable[List, Number.Zero.type, HList.HNil.type, List] =
+    (list: List, _: Number.Zero.type) => (HNil, list)
 
   // split 0:1:2:3 at 3 = split 1:2:3 at 2, left = 0:left, right=right
   implicit def splittable
-  [Head, Tail <: HList, Index <: Number.NonNegative, Left <: HList, Right <: HList]
+  [Head, Tail <: HList, Index <: Number, Left <: HList, Right <: HList]
   (implicit splittable: Splittable[Tail, Index, Left, Right]):
   Splittable[HCons[Head, Tail], Number.Successor[Index], HCons[Head, Left], Right] =
     (list: HCons[Head, Tail], index: Number.Successor[Index]) => {
